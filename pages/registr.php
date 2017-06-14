2b2b4efa66434e99283fd3d574dd0ed4f2517992
webpaul <site@webpaul.ru>
2017-06-14 23:05:24 +0600

    фон и стили фона в шапке

--- pages/registr.php
+++ pages/registr.php
@@ -11,23 +11,32 @@
 		<title>Регистрация ИП, ТОО в Костанае</title>
 
 		<!-- Favicon -->
-		<link rel="icon" type="image/png" sizes="56x56" href="/new/images/fav-icon/icon.png">
+		<link rel="icon" type="image/png" sizes="56x56" href="/images/fav-icon/icon.png">
 
 
 		<!-- Main style sheet -->
-		<link rel="stylesheet" type="text/css" href="/new/css/style.css">
+		<link rel="stylesheet" type="text/css" href="/css/style.css">
 		<!-- responsive style sheet -->
-		<link rel="stylesheet" type="text/css" href="/new/css/responsive.css">
+		<link rel="stylesheet" type="text/css" href="/css/responsive.css">
 
 
 		<!-- Fix Internet Explorer ______________________________________-->
 
 		<!--[if lt IE 9]>
-			<script src="/new/http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-			<script src="/new/vendor/html5shiv.js"></script>
-			<script src="/new/vendor/respond.js"></script>
+			<script src="/http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+			<script src="/vendor/html5shiv.js"></script>
+			<script src="/vendor/respond.js"></script>
 		<![endif]-->
-			
+		<style>
+        .theme-inner-banner .bg {
+    background: url(../images/inner-page/pregistr.jpg) no-repeat center;
+    background-size: cover;
+}
+.theme-inner-banner .bg .opacity {
+    background: rgba(251, 253, 255, 0.66);
+    text-align: center;
+    padding: 75px 0 85px 0;
+}        </style>	
 	</head>
 
 	<body>
@@ -49,7 +58,7 @@
 			<header class="theme-menu-two">
 				
 
-			<?php include($_SERVER['DOCUMENT_ROOT'].'/new/menu.php'); ?>
+			<?php include($_SERVER['DOCUMENT_ROOT'].'/menu.php'); ?>
 			</header> <!-- /.theme-menu-two -->
 
 			
@@ -121,7 +130,7 @@
 									<h4>We Have Great Advisor!!</h4>
 									<p>Here will be somthing that releted with <span class="p-color">business &amp; Consulting</span> services</p>
 								</div>
-								<a href="/new/#" class="tran3s p-bg-color float-right hvr-float-shadow">Free Consultation</a>
+								<a href="/#" class="tran3s p-bg-color float-right hvr-float-shadow">Free Consultation</a>
 							</div> <!-- /.short-advisor-box -->
 						</div> <!-- /.service-middle-text -->
 
@@ -159,43 +168,43 @@
 		<!-- Js File_________________________________ -->
 
 		<!-- j Query -->
-		<script type="text/javascript" src="/new/vendor/jquery.2.2.3.min.js"></script>
+		<script type="text/javascript" src="/vendor/jquery.2.2.3.min.js"></script>
 
 		<!-- Bootstrap JS -->
-		<script type="text/javascript" src="/new/vendor/bootstrap/bootstrap.min.js"></script>
+		<script type="text/javascript" src="/vendor/bootstrap/bootstrap.min.js"></script>
 
 		<!-- Vendor js _________ -->
 		<!-- revolution -->
-		<script src="/new/vendor/revolution/jquery.themepunch.tools.min.js"></script>
-		<script src="/new/vendor/revolution/jquery.themepunch.revolution.min.js"></script>
-		<script type="text/javascript" src="/new/vendor/revolution/revolution.extension.slideanims.min.js"></script>
-		<script type="text/javascript" src="/new/vendor/revolution/revolution.extension.layeranimation.min.js"></script>
-		<script type="text/javascript" src="/new/vendor/revolution/revolution.extension.navigation.min.js"></script>
-		<script type="text/javascript" src="/new/vendor/revolution/revolution.extension.kenburn.min.js"></script>
+		<script src="/vendor/revolution/jquery.themepunch.tools.min.js"></script>
+		<script src="/vendor/revolution/jquery.themepunch.revolution.min.js"></script>
+		<script type="text/javascript" src="/vendor/revolution/revolution.extension.slideanims.min.js"></script>
+		<script type="text/javascript" src="/vendor/revolution/revolution.extension.layeranimation.min.js"></script>
+		<script type="text/javascript" src="/vendor/revolution/revolution.extension.navigation.min.js"></script>
+		<script type="text/javascript" src="/vendor/revolution/revolution.extension.kenburn.min.js"></script>
 		<!-- Mega menu  -->
-		<script type="text/javascript" src="/new/vendor/Menuzord/js/menuzord.js"></script>
+		<script type="text/javascript" src="/vendor/Menuzord/js/menuzord.js"></script>
 		<!-- Bootstrap Select JS -->
-		<script type="text/javascript" src="/new/vendor/bootstrap-select/dist/js/bootstrap-select.js"></script>
+		<script type="text/javascript" src="/vendor/bootstrap-select/dist/js/bootstrap-select.js"></script>
 		
 		<!-- WOW js -->
-		<script type="text/javascript" src="/new/vendor/WOW-master/dist/wow.min.js"></script>
+		<script type="text/javascript" src="/vendor/WOW-master/dist/wow.min.js"></script>
 		<!-- owl.carousel -->
-		<script type="text/javascript" src="/new/vendor/owl-carousel/owl.carousel.min.js"></script>
+		<script type="text/javascript" src="/vendor/owl-carousel/owl.carousel.min.js"></script>
 		<!-- js count to -->
-		<script type="text/javascript" src="/new/vendor/jquery.appear.js"></script>
-		<script type="text/javascript" src="/new/vendor/jquery.countTo.js"></script>
+		<script type="text/javascript" src="/vendor/jquery.appear.js"></script>
+		<script type="text/javascript" src="/vendor/jquery.countTo.js"></script>
 		<!-- fancy box -->
-		<script type="text/javascript" src="/new/vendor/fancy-box/jquery.fancybox.pack.js"></script>
+		<script type="text/javascript" src="/vendor/fancy-box/jquery.fancybox.pack.js"></script>
 		
 		<!-- js count to -->
-		<script type="text/javascript" src="/new/vendor/jquery.appear.js"></script>
-		<script type="text/javascript" src="/new/vendor/jquery.countTo.js"></script>
+		<script type="text/javascript" src="/vendor/jquery.appear.js"></script>
+		<script type="text/javascript" src="/vendor/jquery.countTo.js"></script>
 		<!-- MixitUp -->
-		<script type="text/javascript" src="/new/vendor/jquery.mixitup.min.js"></script>
+		<script type="text/javascript" src="/vendor/jquery.mixitup.min.js"></script>
 
 
 		<!-- Theme js -->
-		<script type="text/javascript" src="/new/js/theme.js"></script>
+		<script type="text/javascript" src="/js/theme.js"></script>
 
 		</div> <!-- /.main-page-wrapper -->
 	</body>
