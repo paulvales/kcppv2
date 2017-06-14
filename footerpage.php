288dc0a57ae26a3efadaac844cf5c61a32f8622c
webpaul <site@webpaul.ru>
2017-06-14 23:20:59 +0600

    иии

---
+++ footerpage.php
@@ -0,0 +1,40 @@
+<footer>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-sm-6">
+							<div class="footer-logo">
+								<a href="/"><img src="/images/logo/logo2.png" alt="Logo"></a>
+								<p>Услуги для вашего бизнеса. Консультируем. Избавляем от бухгалтерских забот. Автоматизация бизнеса.</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-4 footer-list">
+							<h5>Полезные ссылки</h5>
+							<ul>
+								<li><a href="https://egov.kz/" target="_blank" class="tran3s">Egov.kz</a></li>
+								<li><a href="https://client.kazynashylyk.kz" target="_blank" class="tran3s">Казначейство-Клиент</a></li>
+								<li><a href="http://www.goszakup.gov.kz" target="_blank" class="tran3s">Госзакупки</a></li>
+							</ul>
+						</div>
+						<div class="col-lg-2 col-md-2 col-sm-1 footer-list fix">
+							
+						</div>
+						<div class="col-lg-4 col-md-5 col-xs-12 follow-us">
+							<h5>Пообщаемся в соц сетях?</h5>
+							<ul>
+								<li><a href="https://vk.com/10kcpp" target="_blank" class="tran3s"><i class="fa fa-vk" aria-hidden="true"></i></a></li>
+								<li><a href="https://www.instagram.com/rcpp__kosta/" target="_blank" class="tran3s"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+							</ul>
+							<h5>Подписывайтесь</h5>
+							<p>С помощью этой рассылки вы будете в курсе новостей и изменений для вашей деятельности</p>
+							<form action="#" class="subscribe-form">
+								<input type="email" placeholder="Ваш e-mail">
+								<button class="tran3s p-bg-color round-border hvr-icon-wobble-horizontal"><i class="fa fa-angle-right" aria-hidden="true"></i></button>
+							</form>
+						</div>
+					</div> <!-- /.row -->
+
+					<div class="bottom-footer clearfix">
+						<?php include($_SERVER['DOCUMENT_ROOT'].'/footer.php'); ?>
+					</div> <!-- /.bottom-footer -->
+				</div> <!-- /.container -->
+			</footer>
